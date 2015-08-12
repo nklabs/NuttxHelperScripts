0dacc4b9d7def0c09df96be9f855f00404eb856d
Yvan Pearson <ypearson@nklabs.com>
2015-08-12 17:40:11 -0400

    more tweaks for nklabs nuttx helper scripts.

--- build.sh
+++ build.sh
@@ -7,7 +7,6 @@ FLASH="flashrom --programmer dediprog -w $BIN"
 TARGET="ara bridge/es1-debug-apbridgea-nklabs"
 BUILD="build_ara_image.sh $TARGET"
 
-source $HOME/projectara/setupenv.source
+source setupenv.source
 cd nuttx
-
 ./$BUILD && $TRUNCATE && $FLASH
--- getReady.sh
+++ getReady.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
+
+if [ ! -d arm ]; then
+  URL=https://launchpad.net/gcc-arm-embedded/4.8/4.8-2014-q3-update/+download/gcc-arm-none-eabi-4_8-2014q3-20140805-linux.tar.bz2
+  mkdir -p arm
+  wget -qO- $URL | tar -C arm/ -xjf -
+fi
+
 if [ ! -d manifesto ]; then
   git clone https://github.com/projectara/manifesto
 fi
