5a5d19f283522055c02fd79eadcbcf4eb3b1e039
Yvan Pearson <ypearson@nklabs.com>
2015-08-12 13:09:16 -0400

    Added build script with nklabs patch
    -requires to mod setupenv source file for toolchain.

---
+++ build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -x
+#configs/ara/bridge/es1-debug-apbridgea/defconfig
+TARGET="ara bridge/es1-debug-apbridgea"
+BUILD=build_ara_image.sh
+
+if [ ! -d nuttx/apps/ara/nklabs ]; then
+  git clone -b  projectara_nuttx_nklabs_apps git@github.com:nklabs/NuttxApps.git nuttx/apps/ara/nklabs
+fi
+
+source $HOME/projectara/setupenv.source
+cd nuttx
+./$BUILD $TARGET 
+
+truncate -s 2M build/ara-bridge-es1-debug-apbridgea/image/nuttx.bin
+flashrom  --programmer dediprog -w build/ara-bridge-es1-debug-apbridgea/image/nuttx.bin
