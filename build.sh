acfaa3e8ed3a9487825dd9ea6cf3f85ff6c5b4a1
Yvan Pearson <ypearson@nklabs.com>
2015-08-13 16:05:43 -0400

    Changed target build, es1 was incorrect.

--- build.sh
+++ build.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 set -x
-BIN=build/ara-bridge-es1-debug-apbridgea-nklabs/image/nuttx.bin
+BIN=build/ara-bridge-es2-debug-apbridgea-nklabs/image/nuttx.bin
 TRUNCATE="truncate -s 2M $BIN"
 FLASH="flashrom --programmer dediprog -w $BIN"
-
-TARGET="ara bridge/es1-debug-apbridgea-nklabs"
+TARGET="ara bridge/es2-debug-apbridgea-nklabs"
 BUILD="build_ara_image.sh $TARGET"
 
 source setupenv.source
